67d6e51238e9bb3fca9be758381f5469354c1588
Dmitry Freelancer <dimfreelancerdn@gmail.com>
2020-11-24 15:44:38 +0300

    Урок 2 готово к сдачи

--- script.js
+++ script.js
@@ -20,10 +20,8 @@ console.log(addExpenses.length);
 console.log('Период равен', period, 'месяцев');
 console.log('Цель заработать', mission, 'рублей/долларов/гривен/юани');
 
-console.dir(addExpenses.toLowerCase().slice(', '));
+console.dir(addExpenses.toLowerCase().split(', ')); //разбить строку на элементы массива и вывести в консоль
 
 
-let budgetDay = Math.floor(money / 30);  //
+let budgetDay = Math.floor(money / 30);  // расчёт бюджет на день
 console.log('budgetDay: ', budgetDay);
-
-
