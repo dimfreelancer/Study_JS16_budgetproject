a0a21e2f7acc12db318af04df301887e9ea5aa7a
dim freelancer <dimfreelancerdn@gmail.com>
2020-11-24 23:38:38 +0300

    Урок 3 выполнил все как в обязательном задании

--- script.js
+++ script.js
@@ -4,15 +4,21 @@
  * проект Budget GLO Academy
  */
 
-let money = prompt('Ваш месячный доход', 57000),
+let money = +prompt('Ваш месячный доход', 57000),
     income = 'Фриланс',  //дополнительный доход
-    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'Интернет, Такси, Коммуналка'),  //дополнительные расходы
+    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'Квартплата, Проездной, Кредит, Интернет, Такси, Коммуналка'),  //дополнительные расходы
     deposit = confirm('Есть ли у вас депозит в банке?', true),
     mission = 30000,
     period = 12; //число месяцев
 
 let expenses1, expenses2;
 let amount1, amount2;
+
+expenses1 = prompt('Введите обязательную статью расходов', 'Детский садик');
+amount1 = +prompt('Во сколько это обойдется?', 2500);
+
+expenses2 = prompt('Введите обязательную статью расходов', 'Курсы повышения');
+amount2 = +prompt('Во сколько это обойдется?', 13000);
 
 
 console.log(typeof money);
@@ -25,6 +31,24 @@ console.log('Цель заработать', mission, 'рублей/доллар
 console.dir(addExpenses.toLowerCase().split(', ')); //разбить строку на элементы массива и вывести в консоль
 
 
-let budgetDay = Math.floor(money / 30);  // расчёт бюджет на день
-console.log('budgetDay: ', budgetDay);
+let budgetMonth = money - amount1 - amount2;  //бюджет на месяц = доходы минус расходы
+console.log('Бюджет на месяц budgetMonth: ',  budgetMonth);
 
+let budgetDay = Math.floor(budgetMonth / 30); //расчёт бюджет на день
+console.log('Бюджет на день budgetDay: ', budgetDay);
+
+
+period = Math.ceil(mission / budgetMonth);    //период достижения цели
+console.log('Цель ... будет достигнута за период', period, 'месяцев');
+
+
+
+if (budgetDay < 0) {
+    console.log('Что-то пошло не так');
+} else if (budgetDay < 600) {
+    console.log('У вас средний уровень дохода');
+} else if (budgetDay < 1200) {
+    console.log('К сожалению у вас уровень дохода ниже среднего');
+} else if (budgetDay >= 1200) {
+    console.log('У вас высокий уровень дохода');
+}
