0b5dad584ae330c125558bad4eaa9b87c143bdd6
Dmitry Freelancer <dimfreelancerdn@gmail.com>
2020-11-24 08:20:04 +0300

    Урок 1 добавил комментарий для сдачи

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 'use strict';
+/**
+ * Урок 1 
+ * проект Budget GLO Academy
+ */
 
 let money, 
 	income,
 	addExpenses,
 	deposit,
 	mission,
-	period;
+    period;
 
 alert('Hello this alert');
 
