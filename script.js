0939683b6f2d5764d80cea0407dc22ba7123b7f5
dimfreelancer <dimfreelancerdn@gmail.com>
2020-11-24 20:29:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 'use strict';
 /**
- * Урок 2
+ * Урок 3
  * проект Budget GLO Academy
  */
 
-let money = 57000,  //Доход за месяц
-	income = 'Фриланс',  //дополнительный доход
-	addExpenses = 'Интернет, Такси, Коммуналка',  //дополнительные расходы
-	deposit = true,
-	mission = 30000,
+let money = prompt('Ваш месячный доход', 57000),
+    income = 'Фриланс',  //дополнительный доход
+    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'Интернет, Такси, Коммуналка'),  //дополнительные расходы
+    deposit = confirm('Есть ли у вас депозит в банке?', true),
+    mission = 30000,
     period = 12; //число месяцев
+
+
 
 console.log(typeof money);
 console.log(typeof income);
 console.log(typeof deposit);
 
 console.log(addExpenses.length);
-
 console.log('Период равен', period, 'месяцев');
 console.log('Цель заработать', mission, 'рублей/долларов/гривен/юани');
-
 console.dir(addExpenses.toLowerCase().split(', ')); //разбить строку на элементы массива и вывести в консоль
 
 
 let budgetDay = Math.floor(money / 30);  // расчёт бюджет на день
 console.log('budgetDay: ', budgetDay);
+
