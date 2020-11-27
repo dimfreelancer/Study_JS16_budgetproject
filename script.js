0f32347495973d281d3fa7affc7b548ba6e5ffe4
Dim Dima <dimfreelancerdn@gmail.com>
2020-11-27 13:59:28 +0300

    Урок 4 исправил расположение функций и statusIncome

--- script.js
+++ script.js
@@ -4,6 +4,12 @@
  * проект Budget GLO Academy
  */
 
+//функция показывает тип данных
+function showTypeOf(data) {
+    console.log(data, typeof data);
+}
+
+
 let money = +prompt('Ваш месячный доход', 60000),
     income = 'Фриланс, подработка, частные проекты',  //дополнительный доход
     addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'Квартплата, Проездной, Кредит, Интернет, Такси, Коммуналка'),  //дополнительные расходы
@@ -11,8 +17,8 @@ let money = +prompt('Ваш месячный доход', 60000),
     mission = 30000,
     period = 12; //число месяцев
 
-let expenses, expenses1, expenses2; //обязательные расходы
-let amount, amount1, amount2;     //их стоимость
+let expenses, expenses1, expenses2, //обязательные расходы
+    amount, amount1, amount2;     //их стоимость
 
 let accumulatedMonth,
     budgetDay,
@@ -20,18 +26,12 @@ let accumulatedMonth,
 
 
 
-    //функция показывает тип данных
-function showTypeOf(data) {
-    console.log(data, typeof data);
-}
-
 function getExpensesMonth() {
     //Функция возвращает сумму всех обязательных расходов за месяц
     expenses1 = prompt('Введите обязательную статью расходов', 'Детский садик');
     amount1 = +prompt('Во сколько это обойдется?', 22500);
     expenses2 = prompt('Введите обязательную статью расходов', 'Курсы повышения');
     amount2 = +prompt('Во сколько это обойдется?', 13000);
-
     amount = amount1 + amount2;
     return amount;
 }
@@ -41,7 +41,6 @@ function getAccumulatedMonth() {
     return money - (amount1 + amount2);
 }
 
-
 function getTargetMonth() {
     //Подсчитывает за какой период будет достигнута цель, зная результат месячного накопления
     period = Math.ceil(mission / accumulatedMonth);   
@@ -49,8 +48,9 @@ function getTargetMonth() {
 }
 
 function getStatusIncome() {
-    
-    //оформить чать кода в функцию
+    //оформить чать кода в функцию оценить уровень дохода
+
+/* 
     if (budgetDay < 0) {
         console.log('Что-то пошло не так');
     } else if (budgetDay <= 600) {
@@ -60,6 +60,17 @@ function getStatusIncome() {
     } else if (budgetDay >= 1200) {
         console.log('У вас высокий уровень дохода');
     }
+*/
+    if (budgetDay > 1200) {
+        console.log('У вас высокий уровень дохода');
+    } else if (budgetDay >= 600) {
+        console.log('У вас средний уровень дохода');
+    } else if (budgetDay >= 0) {
+        console.log('К сожалению у вас уровень дохода ниже среднего');
+    } else {
+        console.log('Что-то пошло не так');
+    }
+    
 }
 
 
@@ -67,24 +78,19 @@ showTypeOf(money);
 showTypeOf(income);
 showTypeOf(deposit);
 
-console.log('Цель заработать', mission, 'рублей/долларов/гривен/юани');
-
+console.log(`Период ${period} месяца`);
+console.log('Цель заработать', mission, 'рублей/долларов/гривен/юанив');
 console.log('Расходы за месяц', getExpensesMonth());
 console.log('Возможные расходы:', addExpenses.toLowerCase().split(', ')); //разбиваем строку на элементы массива
-// console.log(addExpenses.length);
+
 
 accumulatedMonth = getAccumulatedMonth();
-console.log('accumulatedMonth: ', accumulatedMonth);
-
 budgetDay = Math.floor(accumulatedMonth / 30); //расчёт бюджет на день
+
+console.log('accumulatedMonth: ', accumulatedMonth);
 console.log('Бюджет на месяц budgetMonth accumulatedMonth: ',  accumulatedMonth);
 console.log('Бюджет на день budgetDay: ', budgetDay);
 console.log('Срок достижения цели равен', getTargetMonth(), 'месяцев');
-// console.log('Цель ... будет достигнута за период', period, 'месяцев');
-//period = Math.ceil(mission / budgetMonth);    //период достижения цели
-// console.log('Срок достижения цели равен', period, 'месяцев');
-//let budgetMonth = money - amount1 - amount2;  //бюджет на месяц = доходы минус расходы
-
 
 
 getStatusIncome();
